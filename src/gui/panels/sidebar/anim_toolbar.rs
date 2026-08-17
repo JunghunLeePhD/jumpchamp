0dd895000e2bdd5fb11b37da8dfe3802bc1883cd
junghun <yijeonghunxd@gmail.com>
2026-08-17 22:43:33 +0000

    fix(gui): replace deprecated clamp_to_range with clamping(egui::SliderClamping::Always)

--- src/gui/panels/sidebar/anim_toolbar.rs
+++ src/gui/panels/sidebar/anim_toolbar.rs
@@ -87,7 +87,7 @@ fn render_speed_fps_slider(ui: &mut egui::Ui, state: &mut AppState) {
         egui::Slider::new(&mut state.anim_speed_fps, 1.0..=120.0)
             .step_by(1.0)
             .suffix(" FPS")
-            .clamp_to_range(true),
+            .clamping(egui::SliderClamping::Always),
     );
 
     if ui.button("30").on_hover_text("Set to 30 FPS").clicked() {
